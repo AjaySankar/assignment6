ed622b694119e46a5dd9ebeea18cac94d876979a
Ajay Sankar <ajay0196@gmail.com>
2020-04-18 23:56:05 -0700

    Applied bootstrap to Delete button

--- ui/src/ProductRow.js
+++ ui/src/ProductRow.js
@@ -2,6 +2,7 @@ import React, { Component } from "react"
 import { graphql } from "react-apollo"
 import { gql } from "apollo-boost"
 import { Link } from "react-router-dom"
+import { Button } from 'react-bootstrap'
 
 const deleteProductMutation = gql`
   mutation removeProduct($id: Int!) {
@@ -55,7 +56,7 @@ class ProductRow extends Component {
           <Link to={`/edit/product/${id}`}> Edit </Link>
         </td>
         <td>
-          <input type="button" value="Delete" onClick={this.handleDelete} />
+          <Button variant="danger" onClick={this.handleDelete}> Delete </Button>
         </td>
       </tr>
     )
